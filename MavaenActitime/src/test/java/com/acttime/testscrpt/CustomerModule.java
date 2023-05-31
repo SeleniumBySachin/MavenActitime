49ea2b1ace87c8e93278c48553cb2e4b755c8b16
DELL <DELL@DESKTOP-EFLV044>
2023-05-31 22:41:02 +0530

    MavenProject

---
+++ MavaenActitime/src/test/java/com/acttime/testscrpt/CustomerModule.java
@@ -0,0 +1,12 @@
+package com.acttime.testscrpt;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerModule {
+	@Test
+	public void createCustomer() {
+	Reporter.log("createCustomer",true);
+	}
+
+}
